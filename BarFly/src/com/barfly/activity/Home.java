3ef89741cad2c6783c5f03ee42487ef365a9ac9f
Arkady Blyakher <blyakher.a@gmail.com>
2011-12-08 07:01:43 -0500

    reimplemented onPostExecute, added 'see all attending' dynamic buttons and views

--- BarFly/src/com/barfly/activity/Home.java
+++ BarFly/src/com/barfly/activity/Home.java
@@ -1,7 +1,24 @@
 package com.barfly.activity;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -16,7 +33,6 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.barfly.GetUser;
 import com.barfly.R;
 import com.barfly.User;
 
@@ -34,7 +50,6 @@ public class Home extends Activity {
         if (state != null) {
         	GetUser getUser = new GetUser();
         	getUser.execute(state.getString("username"));
-        	user = getUser.getUser();
         	user.setName(state.getString("username"));
         }
         
@@ -63,16 +78,29 @@ public class Home extends Activity {
         });	
         
         Button showEvents = (Button) findViewById(R.id.see_attending);
-        // See Attending Events
+        
+        // inactivate
+        showEvents.setEnabled(false);
+        
+        // Join a Barcrawl
         showEvents.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
-        		setContentView(R.layout.event);
-        		eventScreenLoad();
+        		// Pass the name with which to login
+				Intent intent = new Intent(getApplicationContext(), CrawlsInProgress.class);
+				intent.putExtra("username", user.getName());		
+				startActivity(intent);
+				
+        		//setContentView(R.layout.event);
+        		//eventScreenLoad();
         	}
         });
         
         Button showInvites = (Button) findViewById(R.id.see_invites);
-        // See Attending Events
+        
+        // inactivate
+        showInvites.setEnabled(false);
+        
+        // See all Invites
         showInvites.setOnClickListener(new OnClickListener() {
         	public void onClick(View v) {
         		setContentView(R.layout.event);
@@ -130,5 +158,102 @@ public class Home extends Activity {
 	    default:
 	        return super.onOptionsItemSelected(item);
 	    }
+	}
+	
+	public class GetUser extends AsyncTask<String, Void, HashMap<String, Object>> {
+		
+		@Override
+		protected HashMap<String, Object> doInBackground(String... params) {
+			HashMap<String, Object> response = new HashMap<String, Object>();
+			
+			String username = params[0];
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpGet httpget = new HttpGet("http://10.0.2.2:8888/getUser?user="+username);	
+			
+			try {
+				HttpResponse httpresponse = httpclient.execute(httpget);
+				
+				if (httpresponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+					InputStream content = httpresponse.getEntity().getContent();
+					BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+					
+					String s = "";
+					while ((s = buffer.readLine()) != null) {
+
+						if (s.startsWith("<friends>")) {
+							s = s.replace("<friends>", "");
+							s = s.replace("[", "");
+							s = s.replace("]", "");
+							List<String> friends =  Arrays.asList(s.split(","));
+							response.put("friends", friends);
+							
+						}
+						
+						if (s.startsWith("<invites>")) {
+							s = s.replace("<invites>", "");
+							s = s.replace("[", "");
+							s = s.replace("]", "");
+							List<String> invites = Arrays.asList(s.split(","));
+							response.put("invites", invites);
+						}
+						
+						if (s.startsWith("<attending>")) {
+							s = s.replace("<attending>", "");
+							s = s.replace("[", "");
+							s = s.replace("]", "");
+							List<String> attending =  new ArrayList<String>();
+							attending = Arrays.asList(s.split(","));
+							response.put("attending", attending);
+						}
+						
+						if (s.startsWith("<location>")) {
+							s = s.replace("<location>", "");
+							s = s.replace("[", "");
+							s = s.replace("]", "");
+							
+						}
+					}
+				}
+					
+				
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			return response;
+		}
+		
+		@SuppressWarnings("unchecked")
+		protected void onPostExecute(HashMap<String, Object> result) {
+			if (result.containsKey("friends")) {
+				List<String> friends = (List<String>) result.get("friends");
+				for (String friend: friends) {
+					user.addFriend(friend);
+				}
+			}
+			
+			if (result.containsKey("invites")) {
+				user.addInvites((List<String>) result.get("invites"));
+				
+				// Enable our button
+				((Button) findViewById(R.id.see_invites)).setEnabled(true);
+			}
+			
+			if (result.containsKey("attending")) {
+				user.addAttending((List<String>) result.get("attending"));
+				
+				// Enable our button
+				((Button) findViewById(R.id.see_attending)).setEnabled(true);
+				
+			}
+			
+			if (result.containsKey("location")) {
+				//user.setLocation(location)
+			}
+			
+		}
+		
 	}
 }
---
+++ BarFly/src/com/barfly/activity/ViewCrawl.java
@@ -0,0 +1,7 @@
+package com.barfly.activity;
+
+import android.app.Activity;
+
+public class ViewCrawl extends Activity {
+
+}
